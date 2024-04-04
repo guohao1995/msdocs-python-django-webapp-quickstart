ed5e20d24b3a047d47a066be0e53fc48c730cd64
Hao Guo <49539832+guohao1995@users.noreply.github.com>
2024-04-04 11:35:02 -0500

    Update settings.py

--- quickstartproject/settings.py
+++ quickstartproject/settings.py
@@ -51,12 +51,12 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    #'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'quickstartproject.urls'
 
-X_FRAME_OPTIONS = "SAMEORIGIN"
+#X_FRAME_OPTIONS = "SAMEORIGIN"
 
 TEMPLATES = [
     {
